ebba2e5bc8c340c1d15d6c515be949571dcc7950
Shaun Nugent <shaun.n88@gmail.com>
2018-07-05 18:41:06 +0000

    Functionality done. Use txt files for riddles/answers/score. Added winning page. Deleted unused files.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# flask-riddle-game
+# Riddle Me This Game
+
+### Description
+
+In this project I created a simple website with the usual HTML, CSS, Javascript for the front end and Python and Flask for the back end.
+
+The user is required to input their name to start the game, once in, you get asked 10 riddles.
+The maximum score per riddle is 3 points.
+You get 3 guesses per riddle. As you use up your guesses, 1 point is subtracted from that score so if you get one wrong guess you will get 2 points, two wrong guesses is 1 point and three wrong guesses is gameover.
+Once the user makes it to the end of the 10th riddle, their score will be added into the highscore file and if they make the top 10, they will be added to the highscore page.
+
+The site was made using VS Code and Cloud 9 IDE and tested using Google Chrome, Mozilla Firefox and Internet Explorer. Several Android devices as well as an iPhone 6s were also used in the testing process. Google Chrome inspector was used to simulate several device screen sizes I could not personally get my hands on.
+
+
+### The following is a table on the functionality of the site.
+
+| Page/Feature | Description |
+| :--- | :---: |
+| Homepage | Extremely simple, asks for a username in order to continue to the game |
+| Welcome | This is a small page welcoming the user with some rules |
+| About | Quick about page with little info |
+| Highscore | This is where the highscores are kept and viewed |
+| Game | The meat of the project, where all the fun happens |
+| Gameover | If you lose, you will be redirected to this page |
+| Congratulations | If you win, you will be redirected to this page |
+
+
+### Below is a list of Libraries/Technologies that were used in the creation of this website:
+
+* [Bootstrap CSS Framework](https://getbootstrap.com/)
+This framework was used throughout the website on all pages to facilitate a responsive design.
+* [FontAwesome Icons](https://fontawesome.com/)
+Used for the Social Media icons and the Media button.
--- run.py
+++ run.py
@@ -1,96 +1,239 @@
 import os
-import json
-import random
-from datetime import datetime
-from flask import Flask, render_template, redirect, request, flash
+import sys
+from flask import Flask, render_template, redirect, request, url_for
 
+# Needed for encoding to utf8
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
+data = []
 
 
+# Good ol' write to file function
 def write_to_file(filename, data):
-    """handle the process of writing data to a file"""
-    with open(filename, "a") as file:
+    with open(filename, "a+") as file:
         file.writelines(data)
 
 
-def add_answer(username, answer):
-    """Add messages to the 'messages' list"""
-    write_to_file("data/answers.txt", "{0}\n{2}\n".format(
-            username.title(),
-            answer
-            ))
+#This is where the riddles live
+def riddle():
+    riddles = []
+    with open("data/-riddles.txt", "r") as e:
+        lines = e.read().splitlines()
+    for line in lines:
+        riddles.append(line)
+    return riddles
 
 
-def get_all_answers():
-    """Get all of the messages and separate them by a 'br'"""
+# This is where the answers for the riddles live
+def riddle_answers():
     answers = []
-    with open("data/answers.txt", "r") as riddle_answers:
-        answers = riddle_answers.readlines()
+    with open("data/-answers.txt", "r") as e:
+        lines = e.read().splitlines()
+    for line in lines:
+        answers.append(line)
     return answers
 
 
+# Clear functions for wrong answers and score
+def clear_guesses(username):
+    with open("data/user-" + username + "-guesses.txt", "w"):
+        return
+
+def clear_score(username):
+    with open("data/user-" + username + "-score.txt", "w"):
+        return
+
+
+# Wrong answer handling
+def store_all_attempts(username):
+    attempts = []
+    with open("data/user-" + username + "-guesses.txt", "r") as incorrect_attempts:
+        attempts = incorrect_attempts.readlines()
+    return attempts
+
+def num_of_attempts():
+    attempts = store_all_attempts(username)
+    return len(attempts)
+
+def attempts_remaining():
+    remaining_attempts = 3 - num_of_attempts()
+    return remaining_attempts
+
+
+# Score gets lower the more attempts used
+def add_to_score():
+    round_score = 4 - num_of_attempts()
+    return round_score
+
+#Adds all the scores from all riddles to make final score
+def end_score(username):
+    with open("data/user-" + username + "-score.txt", "r") as numbers_file:
+        total = 0
+        for line in numbers_file:
+            try:
+                total += int(line)
+            except ValueError:
+                pass
+    return total
+
+#Add final score to highscore list after the last riddle
+def final_score(username):
+    score = str(end_score(username))
+    
+    if username != "" and score != "":
+        with open("data/-highscores.txt", "a") as file:
+                file.writelines(username + "\n")
+                file.writelines(score + "\n")
+    else:
+        return
+
+#Used to retrieve scores from highscore file for use on highscore page
+def get_scores():
+    usernames = []
+    scores = []
+
+    with open("data/-highscores.txt", "r") as file:
+        lines = file.read().splitlines()
+    # Separates usernames and scores
+    for i, text in enumerate(lines):
+        if i%2 ==0:
+            usernames.append(text)
+        else:
+            scores.append(text)
+    # Sorts and zips all the highscore info up for use on highscore page
+    usernames_and_scores = sorted(zip(usernames, scores), key=lambda x: x[1], reverse=True)
+    return usernames_and_scores
+
+
+
+
+# HOMEPAGE
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        with open("data/online_users.txt", "r+") as file:
-            for line in file: # 2: If name already exists in file, skip this and redirect to username page
-                if request.form["username"].lower() in line:
-                    return redirect(request.form['username'].lower())
-            else: # 1: if name is not in file, add it then redirect to username page
-                file.write(request.form["username"].lower() + "\n")
-        return redirect(request.form['username']) # 3: redirect to username page
+        global username
+        username = request.form['username'].lower()
+        if username == "":
+            return render_template("index.html", page_title="Home", username=username)
+        else:
+            return redirect(url_for('user', username=username))
     return render_template("index.html", page_title="Home")
 
 
+# USER WELCOME PAGE
+@app.route('/<username>', methods=["GET", "POST"])
+def user(username):
+
+    # Create a User Specific File for Score Keeping etc.
+    open("data/user-" + username + "-score.txt", 'a').close()
+    clear_score(username)
+    open("data/user-" + username + "-guesses.txt", 'a').close()
+    clear_guesses(username)
+    
+    if request.method =="POST":
+        return redirect(url_for('game', username=username))
+
+    return render_template("welcome.html",
+                            username=username)
+
+
+# GAME PAGE
+@app.route('/<username>/game', methods=["GET", "POST"])
+def game(username):
+    
+    remaining_attempts = 3
+    riddles = riddle()
+    riddle_index = 0
+    answers = riddle_answers()
+    score = 0
+    
+    if request.method == "POST":
+
+        riddle_index = int(request.form["riddle_index"])
+        user_response = request.form["answer"].title()
+        
+        write_to_file("data/user-" + username + "-guesses.txt", user_response + "\n")
+        
+        # Compare the user's answer to the correct answer of the riddle
+        if answers[riddle_index] == user_response:
+            # Correct answer
+            if riddle_index < 9:
+                # If riddle number is less than 10 & answer is correct: add score, clear wrong answers file and go to next riddle
+                write_to_file("data/user-" + username + "-score.txt", str(add_to_score()) + "\n")
+                clear_guesses(username)
+                riddle_index += 1
+            else:
+                # If right answer on LAST riddle: add score, submit score to highscore file and redirect to congrats page
+                write_to_file("data/user-" + username + "-score.txt", str(add_to_score()) + "\n")
+                final_score(username)
+                return redirect(url_for('congrats', username=username, score=end_score(username)))
+
+        else:
+            # Incorrect answer
+            if attempts_remaining() > 0:
+                # if answer was wrong and more than 0 attempts remaining, reload current riddle
+                riddle_index = riddle_index
+            else:
+                # If all attempts are used up, redirect to Gameover page
+                return redirect(url_for('gameover', username=username))
+
+    return render_template("game.html",
+                            username=username, riddle_index=riddle_index, riddles=riddles, 
+                            answers=answers, attempts=store_all_attempts(username), remaining_attempts=attempts_remaining(), score=end_score(username))
+
+
+# GAMEOVER PAGE
+@app.route('/<username>/gameover', methods=["GET", "POST"])
+def gameover(username):
+    
+    clear_guesses(username)
+    clear_score(username)
+    
+    rem_attempts = 3
+    riddles = riddle()
+    riddle_index = 0
+    answers = riddle_answers()
+    score = 0
+    
+    if request.method =="POST":
+        
+        return redirect(url_for('game', username=username, riddle_index=riddle_index, riddles=riddles, 
+                            answers=answers, attempts=store_all_attempts(username), remaining_attempts=attempts_remainder(), score=end_score(username)))
+
+    return render_template("gameover.html",
+                            username=username)
+
+
+# FINISH PAGE
+@app.route('/<username>/congratulations', methods=["GET", "POST"])
+def congrats(username):
+    
+    clear_guesses(username)
+    
+    if request.method =="POST":
+        usernames_and_scores = get_scores()
+        return redirect(url_for('highscores', usernames_and_scores=usernames_and_scores))
+
+    return render_template("congratulations.html",
+                            username=username, score=end_score(username))
+                            
+                            
+# ABOUT PAGE
 @app.route('/about')
 def about():
     return render_template("about.html", page_title="About")
 
 
+# HIGHSCORE PAGE
 @app.route('/highscores')
 def highscores():
-    data = []
-    with open('data/highscores.json', 'r') as json_data:
-        data = json.load(json_data)
-        
-    return render_template("highscores.html", page_title="Highscores", highscores=data)
 
+    usernames_and_scores = get_scores()
 
-@app.route("/<username>", methods=["GET", "POST"])
-def user(username):
-    
-    with open("data/riddles.json", "r") as json_data:
-        data = json.load(json_data)
-            
-    if request.method == "POST" and username == username:
-
-        return render_template("game.html",
-                                page_title="Game",
-                                riddles=data)
-                                
-    return render_template("welcome.html",
-                            username=username)
-
-
-# @app.route('/about/<member_name>')
-# def about_member(member_name):
-#     member = {}
-    
-#     with open('data/company.json', 'r') as json_data:
-#         data = json.load(json_data)
-#         for obj in data:
-#             if obj["url"] == member_name:
-#                 member = obj
-    
-#     return render_template("member.html", member=member)
-
-
-# @app.route('/contact', methods=['GET', 'POST'])
-# def contact():
-#     if request.method == "POST":
-#         flash("Thanks {}, we have recieved your message!".format(request.form["name"]))
-#     return render_template("contact.html", page_title="Contact")
+    return render_template("highscores.html", page_title="Highscores", usernames_and_scores=usernames_and_scores)
 
 
 if __name__ == '__main__':
